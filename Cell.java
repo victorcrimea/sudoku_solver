49af7efc443753d1c17076f6f756061efd54b3ae
victorcrimea <victor_crimea@mail.ru>
2015-09-23 21:48:12 +0300

    Cell class added to represent one sudoku cell

---
+++ Cell.java
@@ -0,0 +1,26 @@
+
+public class Cell {
+	private int i;
+	private int j;
+	
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+
+	public int getJ() {
+		return j;
+	}
+
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+	public Cell(int i, int j){
+		this.i = i;
+		this.j = j;
+	}
+}
